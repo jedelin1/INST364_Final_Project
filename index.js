8eabb7ca69fb6ddd3618a06520c3cba55720d35f
Jacob Edelin <jedelin1@terpmail.umd.edu>
2023-05-12 18:38:15 -0400

    Made changes to all files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const open = document.getElementById('open');
+/*const open = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
 
@@ -9,4 +9,16 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
     modal_container.classList.remove('show');
 });
+*/
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
 
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    });
+});
